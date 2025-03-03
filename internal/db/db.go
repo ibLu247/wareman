75fdebd99ff5ca0067e6c422a9501ba90d5aacf5
Ilya <ilyalubenchenko@gmail.com>
2025-03-03 23:44:05 +0400

    подключение в бд через переменную окружения

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"os"
 
 	"github.com/ibLu247/wareman.git/internal/logger"
 	"github.com/jackc/pgx/v5"
@@ -11,7 +12,7 @@ var Conn *pgx.Conn
 
 func ConnectDB() {
 	var err error
-	Conn, err = pgx.Connect(context.Background(), "postgres://postgres:password@localhost:5432/postgres")
+	Conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		logger.Logger.Fatal("Не удалось подключиться к бд")
 	}
